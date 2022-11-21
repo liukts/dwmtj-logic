41223e3a36de8a0e034ff3c95fe073b99e5bc56b
nzogbi <nzogbi@utexas.edu>
2022-11-21 10:17:20 -0600

    Figure Size Fix

--- get_DW_dynamics_FO.py
+++ get_DW_dynamics_FO.py
@@ -29,7 +29,7 @@ J_resets1 = J_resets / 2
 
 
 
-fig,ax = plt.subplots(1,1,figsize=(10,8))
+fig,ax = plt.subplots(1,1,figsize=(8,8))
 
 strings = ["5e5", "4.85e5", "4.7e5"]
 t_sample = np.arange(0,t_pulse + t_rest,0.05e-9)
@@ -46,7 +46,7 @@ ax.set_ylim([0,19])
 ax.set_xlabel('Time (ns)',fontsize=FS,fontname="Arial")
 ax.set_ylabel(r'J$_2$ (A/m$^2$)',fontsize=FS,fontname="Arial")
 ax.set_xticks([0,1,2,3,4])
-ax.set_xticklabels(["0","","2","","4"],fontname="Arial")
+ax.set_xticklabels(["1","2","3","4","5"],fontname="Arial")
 # ax.legend(loc='lower right', prop={'size': FS})
 ax.set_yticks([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
 ax.set_yticklabels(["","1","","3","","5","","7","","9","","11","","13","","15","","17","","19"],fontname="Arial")
--- get_DW_dynamics_Half.py
+++ get_DW_dynamics_Half.py
@@ -35,7 +35,7 @@ J_resets = J_resets1 + J_resets2
 
 
 
-fig,ax = plt.subplots(1,1,figsize=(10,8))
+fig,ax = plt.subplots(1,1,figsize=(8,8))
 
 strings = ["5e5", "4.85e5", "4.7e5"]
 t_sample = np.arange(0,t_pulse + t_rest,0.05e-9)
@@ -53,7 +53,7 @@ ax.set_ylim([0,7])
 ax.set_xlabel('Time (ns)',fontsize=FS,fontname="Arial")
 ax.set_ylabel(r'J$_2$ (A/m$^2$)',fontsize=FS,fontname="Arial")
 ax.set_xticks([0,1,2,3,4])
-ax.set_xticklabels(["0","","2","","4"],fontname="Arial")
+ax.set_xticklabels(["1","2","3","4","5"],fontname="Arial")
 # ax.legend(loc='lower right', prop={'size': FS})
 ax.set_yticks([0,1,2,3,4,5,6,7])
 ax.set_yticklabels(["","1","","3","","5","","7"],fontname="Arial")
